1bdc3c3e8b7910a10108dac952f8a7987f205688
SebMen22 <sebastian.menesesm@uao.edu.co>
2026-05-09 10:35:22 -0500

    aaa

---
+++ VRMagana/Assets/sCRIPT/SockerPersonal.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class SockerPersonal : XRSocketInteractor
+{
+    public string targetTag;
+    public override bool CanHover(IXRHoverInteractable interactable) 
+    { 
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+    public override bool CanSelect(IXRSelectInteractable interactable) 
+    { 
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
+}
